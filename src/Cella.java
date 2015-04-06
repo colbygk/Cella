ac69de556761e804771bcfe285bffb6757ecf165
Colby Gutierrez-Kraybill <hgranger@unm.edu>
2015-04-06 11:47:53 -0600

    license info for log4j

--- src/Cella.java
+++ src/Cella.java
@@ -2,3 +2,15 @@
 /*
  * Cella.java
  */
+
+import java.io.PrintStream;
+
+public class Cella
+{
+  public static void main ( String [] args )
+  {
+    PrintStream o = System.out;
+
+    o.println("Hi");
+  }
+}
