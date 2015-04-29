751c17d7ee1eb87068b4b9f2fa511e525cf7e79c
Colby Gutierrez-Kraybill <colby@kraybill.com>
2015-04-28 18:52:00 -0600

    What was required to make stella work

--- matlab/lambda_plots.m
+++ matlab/lambda_plots.m
@@ -13,7 +13,7 @@ fnt = 20;
 fnl = 18;
 fnx = 15;
 
-x=load('data/lambda056.dat');
+x=load('data/rho0_correctness_r2.dat');
 
 f1=figure(1);
 plot(x(:,1),x(:,2));
@@ -21,27 +21,22 @@ xlim([0.0 1.0]);
 ylim([0.0 1.0]);
 xlabel('$\rho_0$','Interpreter','Latex','FontSize',fnl);
 ylabel('Fraction of correct classifications','Interpreter','Latex',...
-    'FontSize',fnl);
-title('Correctness of Rule, radius ',...
-    'Interpreter','Latex','FontSize',fnt);
+    'FontSize',fnl );
+title('Correctness of Elite Rules','Interpreter','Latex',...
+    'FontSize',fnt);
 set(gca,'FontSize',fnx);
 
 hold on;
 
-x=load('data/lambda05.dat');
+x=load('data/rho0_correctness_r3.dat');
+plot(x(:,1),x(:,2),'-.');
 
-plot(x(:,1),x(:,2));
-xlim([0.0 1.0]);
-ylim([0.0 1.0]);
-xlabel('$\rho_0$','Interpreter','Latex','FontSize',fnl);
-ylabel('Fraction of correct classifications','Interpreter','Latex',...
-    'FontSize',fnl);
-set(gca,'FontSize',fnx);
+legend( 'r=2, \lambda = 0.0525', 'r=3, \lambda \approx 0.546875',...
+    'Location','Southeast');
 
-legend( '\lambda \approx 0.5625', '\lambda = 0.5','Location','Southeast');
 % Figure saving on OSX seems to be broken in recent versions.
 % this fills a landscape eps.
 f1.PaperOrientation = 'landscape';
 f1.PaperUnits = 'centimeters';
 f1.PaperPosition = [-1.25 1 30 20];
-saveas(f1,'lambda_correctness_1.eps','psc2');
+saveas(f1,'lambda_correctness_plot.eps','psc2');
